aa55bf12d3d3e194591c2dde4554db5ca39f0d13
dayofmolp <molpofdays@gmail.com>
2024-11-21 13:09:37 +0200

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#journey">Our Journey</a></li>
+      <li><a href="#poster">Why I Love Gauta</a></li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
